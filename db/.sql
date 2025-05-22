17daaff22e3233ca147fb4368e5bfcda2af58857
Harisk78 <harisksk78@gmail.com>
2025-05-22 14:40:46 +0530

    login register added online db removed

--- db/.sql
+++ db/.sql
@@ -7,13 +7,3 @@ CREATE TABLE products (
 ALTER TABLE products
 ADD COLUMN parent_id INT DEFAULT NULL,
 ADD CONSTRAINT fk_parent FOREIGN KEY (parent_id) REFERENCES products(id);
-
--- const db = mysql.createConnection({
---   host: 'mysql-2ebaaef9-ecommerce-construction-1.c.aivencloud.com',
---   user: 'avnadmin',
---   password: 'AVNS_LDkcNv993LfkzZNEvkR',
---   database: 'defaultdb',
---   ssl: {
---     ca: fs.readFileSync('C:\E-commerce_Construction\ca.pem')  // Path to downloaded CA cert
---   }
--- });
